682eeeca6ddfd1d74a9e3854b9bf23b4cb40a73d
Luciano DeBortoli <luciano.debortoli31@stclairconnect.ca>
2020-10-01 14:03:52 -0400

    Halfway Mark week 2

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -8,3 +8,43 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'learning-angular';
 }
+
+let name: string;
+
+name = 'Peter';
+
+
+let otherName = 'Luciano';
+
+let isTypeScriptAwesome: boolean;
+
+isTypeScriptAwesome = false;
+
+function doAThing(): void{
+  return;
+}
+
+let somethingRandom: any;
+
+somethingRandom = 1000;
+
+interface Content {
+  id: number;
+  body: string;
+  type: string;
+  imageUrl: string;
+}
+
+const newsArticle: Content = {
+  id: 0,
+  body: 'This just in! Things and stuff!',
+  type: 'news',
+  imageUrl: 'https://my.stclaircollege.ca/sites/student/SiteAssets/Logos/myStclairLogo.png'
+};
+
+function displayTheBody(content: Content): void{
+  console.log(content.body);
+}
+
+displayTheBody(newsArticle);
+
