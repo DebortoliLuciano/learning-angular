ce8bdb1eea4e641f983faf555de610f135dfd29d
Luciano DeBortoli <luciano.debortoli31@stclairconnect.ca>
2020-10-08 14:48:35 -0400

    End of week 3

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -1,71 +1,53 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learning-angular';
+  awesomesauce = '<h1>This is a header!</h1>';
+  gamesList: Content[];
+  constructor() {
+    this.gamesList = [{
+      id: 0,
+      title: 'Modern Warfare 2',
+      imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
+      body: 'This game lived for too long',
+      genre: 'FPS'
+    }, {
+      id: 1,
+      title: 'Far Cry 5',
+      imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
+      genre: 'FPS',
+      body: 'Are we on an island again'
+    }, {
+      id: 2,
+      title: 'Dark Souls',
+      genre: 'Action RPG',
+      body: 'Praise the sun!',
+      imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png'
+    }, {
+      id: 3,
+      title: 'Need for Speed: Heat',
+      genre: 'Racer',
+      body: 'Zoom Zoom',
+      imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png'
+    }, {
+      id: 4,
+      title: 'Super Mario 3D All Stars',
+      genre: '3D Platformer',
+      body: 'Time to find those weird checkered cubes in sunshine',
+      imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png'
+    }];
+  }
 }
 
-let name: string;
-
-name = 'Peter';
-
-
-let otherName = 'Luciano';
-
-let isTypeScriptAwesome: boolean;
-
-isTypeScriptAwesome = false;
-
-function doAThing(): void{
-  return;
-}
-
-let somethingRandom: any;
-
-somethingRandom = 1000;
-
-interface Content {
+export interface Content {
   readonly id: number;
+  title: string;
   body?: string;
-  type: string;
+  genre: string;
   imageUrl: string;
 }
-
-const newsArticle: Content = {
-  id: 0,
-  type: 'news',
-  imageUrl: 'https://my.stclaircollege.ca/sites/student/SiteAssets/Logos/myStclairLogo.png'
-};
-
-newsArticle.body = 'This is the body';
-
-
-function displayTheBody(content: Content): void{
-  console.log(content.body);
-}
-
-displayTheBody(newsArticle);
-
-
-class ContentList {
-  static contentCount = 0;
-  private _items: Content[];
-  constructor(item: Content) {
-    this._items[0] = item;
-    this.increaseCount();
-  }
-  get items(): Content[]{
-    return this._items;
-  }
-  increaseCount() {
-    return ++ContentList.contentCount;
-  }
-}
-
-let newsListOfContent: ContentList;
-newsListOfContent = new ContentList(newsArticle);
-console.log(newsListOfContent.items[0]);
---
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Content} from '../app.component';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+
+  @Input() game: Content;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
