1dd0d29b12f170c03488b100608e33b7c1847a68
Luciano DeBortoli <luciano.debortoli31@stclairconnect.ca>
2020-10-09 15:31:00 -0400

    Done Assignment 2

--- learning-angular/src/app/content-card/content-card.component.ts
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ContentList} from '../helper-files/content-list';
+
 
 
 
@@ -9,32 +9,9 @@ import {ContentList} from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-
-  contentList: ContentList;
   outputHtml: string;
 
   constructor() {
-    this.contentList = new ContentList();
-    this.contentList.addContent({
-      id: 0,
-      author: 'Luciano DeBortoli',
-      title: 'Number 1',
-      body: 'This is a body'
-    });
-    this.contentList.addContent({
-      id: 1,
-      author: 'Luciano DeBortoli',
-      title: 'Number 2',
-      body: 'This is a body'
-    });
-    this.contentList.addContent({
-      id: 2,
-      author: 'Luciano DeBortoli',
-      title: 'Number 3',
-      body: 'This is a body'
-    });
-    this.outputHtml = this.contentList.selectContent(1);
-    console.log(this.contentList.arraySize());
   }
 
   ngOnInit(): void {
---
+++ learning-angular/src/app/content-list/content-list.component.ts
@@ -0,0 +1,60 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Content} from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent implements OnInit {
+  contentList: Content[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.contentList = [{
+      id: 0,
+      author: 'Luciano DeBortoli',
+      imgUrl: 'https://pics.me.me/thumb_pivot-beam-counterweight-sling-frame-payload-diwhy-your-very-own-47202240.png',
+      type: 'Meme',
+      title: 'Alex Trebuchet',
+      body: 'The Great Canadian War Machine',
+      tags: ['Tag1']
+    }, {
+      id: 1,
+      author: 'Luciano DeBortoli',
+      imgUrl: 'https://i.redd.it/1xu0gd5aso411.png',
+      type: 'Meme',
+      title: 'Best War Machine',
+      body: 'You cant compete',
+      tags: ['Tag2']
+    }, {
+      id: 2,
+      author: 'Luciano DeBortoli',
+      imgUrl: 'https://runt-of-the-web.com/wordpress/wp-content/uploads/2017/02/how-would-trebuchets-wear-pants.jpg',
+      type: 'Meme',
+      title: 'Classy Trebuchet',
+      body: 'Its the first way',
+      tags: ['Tag1']
+    }, {
+      id: 3,
+      author: 'Luciano DeBortoli',
+      imgUrl: 'https://i.redd.it/c352lpq157811.jpg',
+      type: 'Meme',
+      title: 'Lovely',
+      body: 'Just Lovely',
+      tags: ['Tag3']
+    }, {
+      id: 4,
+      author: 'Luciano DeBortoli',
+      imgUrl: 'https://i.kym-cdn.com/photos/images/newsfeed/001/276/082/b2c.png',
+      type: 'Meme',
+      title: 'Best Font',
+      body: 'Not entirelly sure what I am supposed to be putting into these objects',
+      tags: ['Tag1']
+    }];
+  }
+
+  onClickMe(id: number): void {
+    console.log(id);
+  }
+}
